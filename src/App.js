ec960ca868d3c23fd43053f800a904b4a05fde3e
Darshan Chhetri <mitch4827@gmail.com>
2024-06-06 14:34:06 +0545

    dark

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import KYC from "./components/KYC";
 import Navbar from "./components/Navbar";
 import Footer from "./section/Footer";
 import Services from "./section/Services";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Hero from "./section/Hero";
 import About from "./section/About";
 import Career from "./section/Career";
